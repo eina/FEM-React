2e85035c215b8280b08333318aacac8e9a23fc7d
thelittleblacksmith <einers@outlook.com>
2017-08-06 23:37:44 -0700

    added babel + basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  // refer to the root directory of your project
+  context: __dirname,
+  // the front door to your project/main file to read from
+  entry: './js/ClientApp.jsx',
+  // inline all my code in production
+  // show pre-transpiled code in errors
+  devtool: 'cheap-eval-source-map',
+  
+  output: {
+    path: path.join(__dirname, 'public'),    
+    filename: 'bundle.js'
+  },
+  
+  resolve: {
+    // order of filenames to see if [filename].[extension] exists
+    extensions: ['.js', '.jsx', '.json']
+  },
+  
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+
+  module: {
+    // apply different loaders to code whooo
+    rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader' }
+    ]
+  }
+
+}
